c4a27ccffca313d178009d5ef1bab0d4180bab5d
srahlbaba <srahlbaba@gmail.com>
2015-08-31 18:48:12 -0400

    phase one

---
+++ src/CreateAFileAndWrite.java
@@ -0,0 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+public class CreateAFileAndWrite {
+	public static void main(String[] args) {
+
+		Writer writer = null;
+
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream("temp.txt"), "utf-8"));
+
+			writer.write("something");
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
